a5e991d5a100089bf047eb03dd78572a05dba3ec
1amacio1 <124075447+1amacio1@users.noreply.github.com>
2024-02-01 23:04:58 +0300

    Add files via upload

---
+++ SiteSchool/Все о компьютере/js/main.js
@@ -0,0 +1,5 @@
+var a=Math.round(Math.random()*1)
+        image = new Array();
+        image[0]="pictures/VSlogo.png"
+        image[1]="pictures/PCHlogo.png"
+        document.write ("<img src=" + image[a] + " class='image' style='weight: 300px; height: 500px'>");
